b8ab05fdf920f469eb50a5d6ea4c8cf576c36d00
jvfsccp <joaovictorfernandescastro@gmail.com>
2026-09-04 19:05:53 -0300

    red: teste item 3 - quantidade maior que estoque lanca EstoqueInsuficienteException

--- src/test/java/carrinho/CarrinhoTest.java
+++ src/test/java/carrinho/CarrinhoTest.java
@@ -3,6 +3,7 @@ package carrinho;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarrinhoTest {
 
@@ -13,12 +14,21 @@ class CarrinhoTest {
     }
 
     @Test
-    void adicionarItemAumentaTotalPeloValorDePrecoVezesQuantidade() {
+    void adicionarItemAumentaTotalPeloValorDePrecoVezesQuantidade() throws EstoqueInsuficienteException {
         Carrinho carrinho = new Carrinho();
         Produto produto = new Produto("Caneta", 2.50, 10);
 
         carrinho.adicionarItem(produto, 3);
 
         assertEquals(7.50, carrinho.calcularTotal(), 0.0001);
+    }
+
+    @Test
+    void adicionarItemComQuantidadeMaiorQueEstoqueLancaExcecao() {
+        Carrinho carrinho = new Carrinho();
+        Produto produto = new Produto("Caneta", 2.50, 5);
+
+        assertThrows(EstoqueInsuficienteException.class,
+            () -> carrinho.adicionarItem(produto, 10));
     }
 }
