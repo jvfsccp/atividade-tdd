2948d2c82af9c8307aa7e00cda4bd1f56d562fac
jvfsccp <joaovictorfernandescastro@gmail.com>
2026-09-04 19:05:07 -0300

    red: teste item 2 - adicionar item aumenta total pelo preco x quantidade

--- src/test/java/carrinho/CarrinhoTest.java
+++ src/test/java/carrinho/CarrinhoTest.java
@@ -11,4 +11,14 @@ class CarrinhoTest {
         Carrinho carrinho = new Carrinho();
         assertEquals(0.0, carrinho.calcularTotal());
     }
+
+    @Test
+    void adicionarItemAumentaTotalPeloValorDePrecoVezesQuantidade() {
+        Carrinho carrinho = new Carrinho();
+        Produto produto = new Produto("Caneta", 2.50, 10);
+
+        carrinho.adicionarItem(produto, 3);
+
+        assertEquals(7.50, carrinho.calcularTotal(), 0.0001);
+    }
 }
